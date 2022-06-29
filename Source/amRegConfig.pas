2307036b8e5d288dbd7ef59a1019755a324454a7
Anders Melander <anders@melander.dk>
2022-06-29 17:53:57 +0200

    Portable config in XML format: - Create registry persistence abstraction layer. - Move existing binary registry persistence to persistence abstraction layer.

--- Source/amRegConfig.pas
+++ Source/amRegConfig.pas
@@ -1648,4 +1648,253 @@ begin
   end;
 end;
 
+type
+  TRegistryReaderWriter = class abstract
+  private
+    FConfiguration: TConfiguration;
+  protected
+    property Configuration: TConfiguration read FConfiguration;
+  public
+    constructor Create(AConfiguration: TConfiguration); virtual;
+  end;
+
+  TRegistryReader = class abstract(TRegistryReaderWriter)
+  private
+  protected
+  public
+    procedure LoadFromStream(Stream: TStream); virtual; abstract;
+  end;
+
+  TRegistryWriter = class abstract(TRegistryReaderWriter)
+  private
+  protected
+  public
+    procedure SaveToStream(Stream: TStream); virtual; abstract;
+  end;
+
+constructor TRegistryReaderWriter.Create(AConfiguration: TConfiguration);
+begin
+  inherited Create;
+  FConfiguration := AConfiguration;
+end;
+
+type
+  TRegistryBinaryReader = class(TRegistryReader)
+  public
+    procedure LoadFromStream(Stream: TStream); override;
+  end;
+
+procedure TRegistryBinaryReader.LoadFromStream(Stream: TStream);
+var
+  Reader: TReader;
+  Reg: TRegistry;
+
+  procedure LoadKey(const Key: string);
+  var
+    RegKey, RegOldKey: HKEY;
+    Name: string;
+    Buffer: Pointer;
+    BufSize: Integer;
+    DataType: Integer;
+  begin
+    if (not Reg.KeyExists(Key)) then
+      Reg.CreateKey(Key);
+
+    RegKey := TRegistryCracker(Reg).GetKey(Key);
+    if (RegKey = 0) then
+      Exit;
+    try
+      RegOldKey := Reg.CurrentKey;
+      try
+        TRegistryCracker(Reg).SetCurrentKey(RegKey);
+
+        // Values
+        Reader.ReadListBegin;
+        while (not Reader.EndOfList) do
+        begin
+          Reader.ReadListBegin;
+          begin
+            Name := Reader.ReadString;
+            DataType := Reader.ReadInteger;
+            BufSize := Reader.ReadInteger;
+            GetMem(Buffer, BufSize);
+            try
+              Reader.Read(Buffer^, BufSize);
+
+              CheckOSError(RegSetValueEx(Reg.CurrentKey, PChar(Name), 0, DataType, Buffer, BufSize));
+            finally
+              FreeMem(Buffer);
+            end;
+          end;
+          Reader.ReadListEnd;
+        end;
+        Reader.ReadListEnd;
+
+        // Relative keys
+        Reader.ReadListBegin;
+        while (not Reader.EndOfList) do
+        begin
+          // Recurse to load keys
+          Name := Reader.ReadString;
+          LoadKey(Name);
+        end;
+        Reader.ReadListEnd;
+
+      finally
+        TRegistryCracker(Reg).SetCurrentKey(RegOldKey);
+      end;
+    finally
+      if (RegKey <> 0) then
+        RegCloseKey(RegKey);
+    end;
+  end;
+
+begin
+  Reader := TReader.Create(Stream, 4096);
+  try
+    if (Reader.ReadInteger <> 1) then
+      raise Exception.Create('Invalid portable registry version');
+
+    if (HKEY(Reader.ReadInteger) <> Configuration.Registry.RootKey) then
+      raise Exception.Create('Invalid portable registry root key');
+
+    Reg := TRegistry.Create(KEY_ALL_ACCESS);
+    try
+      Reg.RootKey := Configuration.Registry.RootKey;
+
+      // RegCopyTree(...);
+      // RegDeleteTree(...);
+
+      Reader.ReadListBegin;
+      begin
+
+        if (Reader.ReadString <> Configuration.KeyPath) then
+          raise Exception.Create('Invalid portable registry root path');
+
+        LoadKey(Configuration.KeyPath);
+
+      end;
+      Reader.ReadListEnd;
+
+      // RegCopyTree(...);
+      // RegDeleteTree(...);
+
+    finally
+      Reg.Free;
+    end;
+  finally
+    Reader.Free;
+  end;
+end;
+
+type
+  TRegistryBinaryWriter = class(TRegistryWriter)
+  public
+    procedure SaveToStream(Stream: TStream); override;
+  end;
+
+procedure TRegistryBinaryWriter.SaveToStream(Stream: TStream);
+var
+  Writer: TWriter;
+  Reg: TRegistry;
+
+  procedure SaveKey(const Key: string);
+  var
+    RegKey, RegOldKey: HKEY;
+    Names: TStrings;
+    Name: string;
+    Buffer: Pointer;
+    BufSize: Integer;
+    DataType: Integer;
+  begin
+    RegKey := TRegistryCracker(Reg).GetKey(Key);
+    if (RegKey = 0) then
+      Exit;
+    try
+      RegOldKey := Reg.CurrentKey;
+      try
+        TRegistryCracker(Reg).SetCurrentKey(RegKey);
+
+        Writer.WriteString(Key); // Key
+
+        Names := TStringList.Create;
+        try
+          // Values
+          Writer.WriteListBegin;
+          begin
+            Reg.GetValueNames(Names);
+            for Name in Names do
+            begin
+              DataType := REG_NONE;
+              CheckOSError(RegQueryValueEx(Reg.CurrentKey, PChar(Name), nil, @DataType, nil, @BufSize));
+
+              GetMem(Buffer, BufSize);
+              try
+                CheckOSError(RegQueryValueEx(Reg.CurrentKey, PChar(Name), nil, @DataType, PByte(Buffer), @BufSize));
+
+                // Single Value
+                Writer.WriteListBegin;
+                  Writer.WriteString(Name); // Name
+                  Writer.WriteInteger(DataType); // Type
+                  Writer.WriteInteger(BufSize); // Value size
+                  Writer.Write(Buffer^, BufSize); // Value
+                Writer.WriteListEnd;
+
+              finally
+                FreeMem(Buffer);
+              end;
+            end;
+          end;
+          Writer.WriteListEnd;
+
+          // Keys relative to current key
+          Writer.WriteListBegin;
+          begin
+            // Recurse to save keys
+            Reg.GetKeyNames(Names);
+            for Name in Names do
+              SaveKey(Name);
+          end;
+          Writer.WriteListEnd;
+
+        finally
+          Names.Free;
+        end;
+      finally
+        TRegistryCracker(Reg).SetCurrentKey(RegOldKey);
+      end;
+    finally
+      if (RegKey <> 0) then
+        RegCloseKey(RegKey);
+    end;
+  end;
+
+begin
+  Writer := TWriter.Create(Stream, 4096);
+  try
+    Writer.WriteInteger(1); // Version
+    Writer.WriteInteger(Integer(Configuration.Registry.RootKey)); // Root
+    Reg := TRegistry.Create(KEY_READ);
+    try
+      Reg.RootKey := Configuration.Registry.RootKey;
+      if (not Reg.OpenKeyReadOnly(Configuration.KeyPath)) then
+        Exit;
+
+      // Absolute keys
+      Writer.WriteListBegin;
+      begin
+
+        SaveKey(Configuration.KeyPath);
+
+      end;
+      Writer.WriteListEnd;
+
+    finally
+      Reg.Free;
+    end;
+  finally
+    Writer.Free;
+  end;
+end;
+
 end.
